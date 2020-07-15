81268d5597a9d4a7752e5cf1834f1e80a68fb17d
jinwoo park <swat018@gmail.com>
2020-07-16 00:10:10 +0900

    JUnit 5: Assertion

--- src/main/java/com/swat018/inflearnthejavatest/Study.java
+++ src/main/java/com/swat018/inflearnthejavatest/Study.java
@@ -2,11 +2,14 @@ package com.swat018.inflearnthejavatest;
 
 public class Study {
 
-    private StudyStatus status = StudyStatus.DRAFT;
+    private StudyStatus status ;
 
     private int limit;
 
     public Study(int limit) {
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+        }
         this.limit = limit;
     }
 
--- src/test/java/com/swat018/inflearnthejavatest/StudyTest.java
+++ src/test/java/com/swat018/inflearnthejavatest/StudyTest.java
@@ -1,11 +1,14 @@
 package com.swat018.inflearnthejavatest;
 
+
 import org.junit.jupiter.api.*;
 
 import java.lang.reflect.Executable;
+import java.time.Duration;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -13,11 +16,29 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 ╯°□°）╯")
     void create_new_study() {
-        Study study = new Study(-10);
-        assertNotNull(study);
-        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT +"상태다.");
-        assertTrue(study.getLimit() > 0, "스터디 최대 참석  가능 인원은 0보다 커야 한다.");
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+
+/*        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+        // TODO ThreadLocal*/
+
+/*        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());*/
+
+ //       Study study = new Study(-10);
+/*        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT +"상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석  가능 인원은 0보다 커야 한다.")
+        );*/
     }
+
 
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
