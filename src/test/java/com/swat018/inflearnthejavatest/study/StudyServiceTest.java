0126d8cd88f3f36978fd99e1cc9b0bd6a2fbe5ec
jinwoo park <swat018@gmail.com>
2020-07-23 06:55:36 +0900

    Mockito - Mock 객체 Stubbing

--- src/test/java/com/swat018/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/com/swat018/inflearnthejavatest/study/StudyServiceTest.java
@@ -36,6 +36,7 @@ class StudyServiceTest {
         member.setId(1L);
         member.setEmail("swat018@gmail.com");
 
+        // 메소드가 동일한 매개변수로 여러번 호출될 때 각기 다르게 행동호도록 조작할 수도 있다.
         when(memberservice.findById(any()))
                 .thenReturn(Optional.of(member))
                 .thenThrow(new RuntimeException())
@@ -56,12 +57,13 @@ class StudyServiceTest {
         Study study = new Study(10, "java");
 
         Optional<Member> findById = memberservice.findById(1L);
-
+        // 특정한 매개변수를 받은 경우 특정한 값을 리턴하거나 예뢰를 던지도록 만들 수 있다.
         assertEquals("swat018@gmail.com", memberservice.findById(1L).get().getEmail());
         assertEquals("swat018@gmail.com", memberservice.findById(2L).get().getEmail());
 
 //        when(memberservice.findById(1L)).thenThrow(new RuntimeException());
 
+        // Void메소드특정매개변수를받거나호출된경우예외를발생시킬수있다.
         doThrow(new IllegalArgumentException()).when(memberservice).validate(1L);
 
         assertThrows(IllegalArgumentException.class, () -> {
