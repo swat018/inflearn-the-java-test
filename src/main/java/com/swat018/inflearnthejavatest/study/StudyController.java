20c2c879903de4d7b0e48ab19f9a2d125a0026b2
jinwoo park <swat018@gmail.com>
2020-08-02 23:44:36 +0900

    성능테스트 - JMeter 소개

---
+++ src/main/java/com/swat018/inflearnthejavatest/study/StudyController.java
@@ -0,0 +1,25 @@
+package com.swat018.inflearnthejavatest.study;
+
+import com.swat018.inflearnthejavatest.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+public class StudyController {
+
+    final StudyRepository repository;
+
+    @GetMapping("/study/{id}")
+    public Study getStudy(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Study not found for '" + id + "'"));
+    }
+
+    @PostMapping("/study")
+    public Study createStudy(@RequestBody Study study) {
+        return repository.save(study);
+    }
+
+}
