d3fd2b56e108566a469e230fee9cfc18951d691f
jinwoo park <swat018@gmail.com>
2022-02-03 17:09:43 +0900

    feat : JUnit 5 - 테스트 반복하기 2부

--- src/main/java/me/swat018/inflearnthejavatest/Study.java
+++ src/main/java/me/swat018/inflearnthejavatest/Study.java
@@ -6,11 +6,18 @@ public class Study {
 
     private int limit;
 
+    private String name;
+
     public Study(int limit) {
         if (limit < 0) {
             throw new IllegalArgumentException("limit는 0보다 커야 한다.");
         }
         this.limit = limit;
+    }
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
     }
 
     public StudyStatus getStatus() {
@@ -19,5 +26,18 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/me/swat018/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/swat018/inflearnthejavatest/StudyTest.java
@@ -2,8 +2,16 @@ package me.swat018.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -82,11 +90,43 @@ class StudyTest {
                 repetitionInfo.getTotalRepetitions());
     }
 
+//    @DisplayName("스터디 만들기")
+//    @ParameterizedTest(name = "{index} {displayName} message={0}")
+////    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+////    @NullAndEmptySource
+//    @ValueSource(ints = {10, 20, 40})
+//    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+//           System.out.println(study.getLimit());
+//    }
+    static class StudyConverter extends SimpleArgumentConverter {
+
+    @Override
+    protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+        assertEquals(Study.class, targetType, "Can only convert to Study");
+        return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+//    @DisplayName("스터디 만들기")
+//    @ParameterizedTest(name = "{index} {displayName} message={0}")
+//    @CsvSource({"10, '자바스터디'","20, '스프링'"})
+//    void parameterizedTest(ArgumentsAccessor argumentsAccessor) {
+//        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+//        System.out.println(study);
+//    }
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
-    void parameterizedTest(String message) {
-        System.out.println(message);
+    @CsvSource({"10, '자바스터디'","20, '스프링'"})
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
     }
 
     @BeforeAll
