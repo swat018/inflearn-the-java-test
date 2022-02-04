881a423e129ebace7bd7cd5d21929c5be2dd00eb
jinwoo park <swat018@gmail.com>
2022-02-04 14:30:11 +0900

    feat : JUnit 5 - JUnit 4 마이그레이션

---
+++ src/test/java/me/swat018/inflearnthejavatest/StudyJUnit4Test.java
@@ -0,0 +1,26 @@
+package me.swat018.inflearnthejavatest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudyJUnit4Test {
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+
+    @Test
+    public void createTest() {
+        System.out.println("test");
+    }
+
+    @Test
+    public void createTest2() {
+        System.out.println("test2");
+    }
+
+}
