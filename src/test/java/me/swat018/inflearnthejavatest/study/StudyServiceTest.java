1a567fefe767006216e8f1971f86dc8b98cbcd18
jinwoo park <swat018@gmail.com>
2022-02-05 21:05:08 +0900

    feat : Testcontainer - 설치

--- src/test/java/me/swat018/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/swat018/inflearnthejavatest/study/StudyServiceTest.java
@@ -3,14 +3,16 @@ package me.swat018.inflearnthejavatest.study;
 import me.swat018.inflearnthejavatest.domain.Member;
 import me.swat018.inflearnthejavatest.domain.Study;
 import me.swat018.inflearnthejavatest.member.MemberService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
@@ -23,11 +25,30 @@ import static org.mockito.Mockito.times;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
+@Testcontainers
 class StudyServiceTest {
 
     @Mock MemberService memberService;
 
     @Autowired StudyRepository studyRepository;
+
+    @Container
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+            .withDatabaseName("studytest");
+
+//    @BeforeAll
+//    static void beforAll() {
+//        postgreSQLContainer.start();
+//    }
+//
+//    @AfterAll
+//    static void afterAll() {
+//        postgreSQLContainer.stop();
+//    }
+    @BeforeEach
+    void beforeEach() {
+        studyRepository.deleteAll();
+    }
 
     @Test
     void createNewStudy() {
